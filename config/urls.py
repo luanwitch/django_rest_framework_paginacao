abff1f88c8d58252a8c4725dc81aec2eb633f16e
Luan Vieira <luanvwittchind@gmail.com>
2025-10-07 09:07:32 -0300

    feat: adiciona apps product e order com models, serializers, views e rotas

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('accounts.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('products/', include('product.urls')),
+    path('orders/', include('order.urls')),
 ]
 
