fcb633ab20073102aa3e356eeab1238af392c470
Luan Vieira <luanvwittchind@gmail.com>
2025-10-22 10:09:49 -0300

    Adiciona teste com 5 produtos usando ProductFactory

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from product import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('products/', include('product.urls')),
     path('orders/', include('order.urls')),
+    path('api-token-auth/', views.obtain_auth_token),
 ]
 
--- order/views.py
+++ order/views.py
@@ -1,7 +1,26 @@
+# order/views.py
+
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
 from .models import Order
-from .serializers import OrderSerializer
+from .serializers import OrderSerializer  
+from .pagination import CustomOrderPagination
 
 class OrderViewSet(viewsets.ModelViewSet):
+
     queryset = Order.objects.all()
-    serializer_class = OrderSerializer
+    serializer_class = OrderSerializer  
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    pagination_class = CustomOrderPagination  
+
+    def get_queryset(self):
+        """
+        This view returns a list of orders filtered for the
+        currently authenticated user.
+        """
+        user = self.request.user
+        
+        return self.queryset.filter(user=user)
