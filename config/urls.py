b175737c3f6af090bb798abf63d14319cceb8f99
Luan Vieira <luanvwittchind@gmail.com>
2025-10-24 09:23:52 -0300

    Testes de POST funcionando localmente com DRF

--- config/urls.py
+++ config/urls.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from rest_framework.authtoken.views import obtain_auth_token
 from product import views
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("API funcionando! 🚀")
 
 urlpatterns = [
+    path('', home),  
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('products/', include('product.urls')),
     path('orders/', include('order.urls')),
-    path('api-token-auth/', views.obtain_auth_token),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
-
